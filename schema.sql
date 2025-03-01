f41ec9aada2a83be614dc868d23becb2aa026d0a
Jerry <jerbjork@users.noreply.github.com>
2025-03-01 22:54:28 +0000

    Remove outdated example data

--- schema.sql
+++ schema.sql
@@ -20,7 +20,6 @@ CREATE TABLE reviews (
     UNIQUE (user_id, title)
 );
 
-
 CREATE TABLE messages (
     id INTEGER PRIMARY KEY,
     content TEXT,
@@ -35,22 +34,3 @@ CREATE TABLE attach (
     review_id INTEGER REFERENCES reviews ON DELETE CASCADE,
     UNIQUE (category_id, review_id)
 );
-
-INSERT INTO categories (title) VALUES ("Adult");
-INSERT INTO categories (title) VALUES ("Adventure");
-INSERT INTO categories (title) VALUES ("Childrens");
-INSERT INTO categories (title) VALUES ("Contemporary");
-INSERT INTO categories (title) VALUES ("Crime");
-INSERT INTO categories (title) VALUES ("Fiction");
-INSERT INTO categories (title) VALUES ("History");
-INSERT INTO categories (title) VALUES ("Horror");
-INSERT INTO categories (title) VALUES ("Mystery");
-INSERT INTO categories (title) VALUES ("Non-fiction");
-INSERT INTO categories (title) VALUES ("Philosophy");
-INSERT INTO categories (title) VALUES ("Poetry");
-INSERT INTO categories (title) VALUES ("Politics");
-INSERT INTO categories (title) VALUES ("Romance");
-INSERT INTO categories (title) VALUES ("Science");
-INSERT INTO categories (title) VALUES ("Science Fiction");
-INSERT INTO categories (title) VALUES ("Young-adult");
-INSERT INTO categories (title) VALUES ("War");
